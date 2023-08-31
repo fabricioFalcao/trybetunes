a37bd248840204a933ec9f260829410d049c0405
fabricioFalcao <fabriciohfd@gmail.com>
2023-08-30 22:05:23 -0300

    Requisito 2

---
+++ src/components/albumCard/AlbumCard.tsx
@@ -0,0 +1,29 @@
+import { Link } from 'react-router-dom';
+import { AlbumType } from '../../types';
+
+function AlbumCard({ artistId, artistName, artworkUrl100, collectionId,
+  collectionName, collectionPrice, releaseDate, trackCount }:AlbumType) {
+  return (
+    <div className="album-card">
+
+      <div className="album-cover">
+        <img src={ artworkUrl100 } alt="Capa do álbum" />
+      </div>
+      <div>
+        <h3>{artistName}</h3>
+        <h4>{collectionName}</h4>
+        <p>{`Preço: ${collectionPrice}`}</p>
+        <p>{`Data de lançamento: ${releaseDate}`}</p>
+        <p>{`Número de faixas: ${trackCount}`}</p>
+      </div>
+      <Link
+        to={ `/album/${collectionId}` }
+        data-testid={ `link-to-album-${collectionId}` }
+      >
+        <button>Ver album</button>
+      </Link>
+    </div>
+  );
+}
+
+export default AlbumCard;
--- src/pages/search/Search.tsx
+++ src/pages/search/Search.tsx
@@ -1,19 +1,75 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import searchAlbumsAPI from '../../services/searchAlbumsAPI';
+import Carregando from '../../components/carregando/Carregando';
+import { AlbumType } from '../../types';
+import AlbumCard from '../../components/albumCard/AlbumCard';
 
 function Search() {
-  const [artist, setArtist] = useState<string>('');
+  const [inputArtist, setInputArtist] = useState('');
+  const [artist, setArtist] = useState('');
+  const [carregando, setCarregando] = useState<boolean>(false);
+  const [renderSearch, setRenderSearch] = useState<boolean>(false);
+  const [albums, setAlbums] = useState<AlbumType[]>([]);
+
+  useEffect(() => {
+    const storedList = JSON.parse(localStorage.getItem('myList'));
+    if (storedList) {
+      setRenderSearch(true);
+      setAlbums(storedList);
+      setInputArtist('');
+      setCarregando(false);
+    }
+  }, []);
+
+  const handleSearch = async () => {
+    setCarregando(true);
+    setArtist(inputArtist);
+    const albumsList = await searchAlbumsAPI(inputArtist);
+    localStorage.setItem('myList', JSON.stringify(albumsList));
+    setRenderSearch(true);
+    setAlbums(albumsList);
+    setInputArtist('');
+    setCarregando(false);
+  };
+
+  if (carregando) {
+    return (<Carregando />);
+  }
 
   return (
-    <div className="search-container">
-      <form action="">
-        <input
-          type="text"
-          data-testid="search-artist-input"
-          value={ artist }
-          onChange={ ({ target }) => setArtist(target.value) }
-        />
-      </form>
-    </div>
+    <>
+      <div className="search-container">
+        <form action="" className="form">
+          <input
+            type="text"
+            data-testid="search-artist-input"
+            value={ inputArtist }
+            onChange={ ({ target }) => setInputArtist(target.value) }
+            placeholder="Nome do artista"
+          />
+          <button
+            data-testid="search-artist-button"
+            type="submit"
+            disabled={ inputArtist.length < 2 }
+            onClick={ handleSearch }
+          >
+            Pesquisar
+          </button>
+        </form>
+      </div>
+      {renderSearch
+      && <div>
+        {albums.length
+          ? <section>
+            <h3>{`Resultado de álbuns de: ${artist}`}</h3>
+            <div className="albums-panel">
+              {albums.map((album) => (
+                <AlbumCard { ...album } key={ album.collectionId } />))}
+            </div>
+          </section>
+          : <h1>Nenhum álbum foi encontrado</h1>}
+         </div>}
+    </>
   );
 }
 
